8382c884ea23938e3db7b9b873f40fa9f120c82c
Jack <jmloizel@gmail.com>
2024-04-27 01:53:31 +0100

    updates

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'Lama Dev Contact', //changes name on tab (static page)
+  description: 'This is the contact page',
+}
+
+export const Contact = () => {
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+export default Contact
