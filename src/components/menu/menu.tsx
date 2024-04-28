35425d06bcb645f6adb4bb552e8d4f050a203864
Jack <jmloizel@gmail.com>
2024-04-28 12:24:15 +0100

    updates

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,12 +1,14 @@
 "use client"
 
+
 import React, { useState } from 'react';
-import { Box, createTheme, useMediaQuery } from '@mui/material';
+import { Box, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import styles from "./page.module.css";
 import ModalClose from '@mui/joy/ModalClose';
 import Button from '@mui/joy/Button';
 import Drawer from '@mui/joy/Drawer';
+import { extendTheme } from '@mui/joy/styles';
 
 interface MenuProps {
     
@@ -30,7 +32,6 @@ const Menu: React.FC<MenuProps> = ({}) => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
   const isTabletOrAbove = useMediaQuery(theme.breakpoints.up('md'));
-  
 
   return (
     <Box className={styles.menu}>
@@ -44,14 +45,13 @@ const Menu: React.FC<MenuProps> = ({}) => {
         </Button>
       </div>
       <Drawer
-        sx={{height: "100%", background: "#434147"}}
+        sx={{height: "100%"}}
         open={open} 
         onClose={() => {
           setOpen(false);
         }}
         anchor= {isMobile ? "right" : "left"}
         size={"lg"}
-        // color=''
       >
         <ModalClose/>
       </Drawer>
