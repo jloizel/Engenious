e77927f233c3d086cb561a9158e4105430d94110
Jack <102869405+jloizel@users.noreply.github.com>
2024-05-06 16:09:20 +0100

    footer updates

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './page.module.css'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer: React.FC = ({}) => {
   
@@ -18,16 +19,32 @@ const Footer: React.FC = ({}) => {
         </button>
       </div>
       <hr className={styles.line}></hr>
-      <div className={styles.logoContainer}>
-        <a href="/">
-          <img className={styles.logo} src="/engenious.png" alt="engenious logo" />
-        </a>
-        <a href="/" className={styles.link}>
-          <div className={styles.companyNameContainer}>
-            <div className={styles.companyName1}>ENGENIOUS</div>
-            <div className={styles.companyName2}>RECRUITMENT</div>
-          </div>
-        </a>
+      <div className={styles.logos}>
+        <div className={styles.logoContainer}>
+          <a href="/">
+            <img className={styles.logo} src="/engenious.png" alt="engenious logo" />
+          </a>
+          <a href="/" className={styles.link}>
+            <div className={styles.companyNameContainer}>
+              <div className={styles.companyName1}>ENGENIOUS</div>
+              <div className={styles.companyName2}>RECRUITMENT</div>
+            </div>
+          </a>
+        </div>
+        {/* <div className={styles.linkedInContainer}>
+          <LinkedInIcon className={styles.linkedIn}/>
+        </div> */}
+      </div>
+      <div className={styles.pagesContainer}>
+        <div className={styles.page}>About Us</div>
+        <div className={styles.page}>Join Us</div>
+        <div className={styles.page}>Contact Us</div>
+        <div className={styles.page}>Upload CV</div>
+        <div className={styles.page}>About Us</div>
+        <div className={styles.page}>Submit Job</div>
+        <div className={styles.page}>Privacy Policy</div>
+        <div className={styles.page}>Join Us</div>
+        <div className={styles.copyright}>© Engenious Recruitment</div>
       </div>
     </div>
   )
