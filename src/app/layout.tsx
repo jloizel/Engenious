8ffcde755de1ecbaff9e2d59201e3617f9637d00
Jack <102869405+jloizel@users.noreply.github.com>
2024-06-09 19:09:51 +0100

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta charSet="UTF-8"/>
-      <body className={inter.className}>
+      <body className="body">
         {children}
         <Footer/>
       </body>
