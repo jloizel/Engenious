ba94fcd9cdb5480773dde2acd5f68b6f517a892c
Jack <102869405+jloizel@users.noreply.github.com>
2024-06-09 19:35:12 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1"/>
       <meta charSet="UTF-8"/>
       <body className="body">
         {children}
