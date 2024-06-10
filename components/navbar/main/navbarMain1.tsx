7c97bd895d2ad6747a77b4c72acd68447b383cbf
Jack <jmloizel@gmail.com>
2024-06-10 22:33:39 +0100

    Update navbarMain1.tsx

--- components/navbar/main/navbarMain1.tsx
+++ components/navbar/main/navbarMain1.tsx
@@ -46,7 +46,7 @@ const NavbarMain1 = () => {
   
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
-  const isTabletOrAbove = useMediaQuery(theme.breakpoints.up('md'));
+  const isTabletOrAbove = useMediaQuery(theme.breakpoints.up('sm'));
   const isTabletOrBelow = useMediaQuery(theme.breakpoints.down('md'));
 
   const setDisplay  = () => {
@@ -73,10 +73,52 @@ const NavbarMain1 = () => {
    }
   }
 
+  const handleContainerWidth = () => {
+    if (isMobile) {
+      return "100%"
+    }
+  }
+
+  const handleContainerJustify = () => {
+    if (isMobile) {
+      return "center"
+    } else {
+      return "space-between"
+    }
+  }
+
+  const handleContainerMargin = () => {
+    if (!isMobile) {
+      return "10px"
+    }
+  }
+
+  const handleContainerColumn = () => {
+    if (!isMobile) {
+      return "20px"
+    }
+  }
+
+  const handleLeftWidth = () => {
+    if (isMobile) {
+      return "90%"
+    } else {
+      return
+    }
+  }
+
+  const handleLeftJustify = () => {
+    if (isMobile) {
+      return "space-between"
+    } else {
+      return
+    }
+  }
+
   return (
     <div className={styles.navbar} style={{width: "100%"}}>
-      <div className={styles.container}>
-        <div className={styles.left} style={isMobile ? {width: "90%", alignItems: "center", justifyContent: "space-between", display: "flex"} : {alignItems: "center", display: "flex"}}>
+      <div className={styles.container} style={{justifyContent: handleContainerJustify(), width: handleContainerWidth(), marginLeft: handleContainerMargin(), marginRight: handleContainerMargin(), columnGap: handleContainerColumn()}}>
+        <div className={styles.left} style={{width: handleLeftWidth(), justifyContent: handleLeftJustify(), display: "flex"}}>
           <div className={styles.menu1} style={{display: setDisplay()}}>
             <Menu color={"white"}/>
           </div>
