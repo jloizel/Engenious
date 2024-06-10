f299cb3885725859c5511988759676970d58a544
Jack <jmloizel@gmail.com>
2024-06-10 20:10:27 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1"/>
       <meta charSet="UTF-8"/>
+      <meta http-equiv='cache-control' content='no-cache'/>
+      <meta http-equiv='expires' content='0'/>
+      <meta http-equiv='pragma' content='no-cache'/>
       <body className="body">
         {children}
         <Footer/>
