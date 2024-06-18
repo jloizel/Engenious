f7acf06403b1a72c442d5bba56494d9f4e4356ab
Jack <jmloizel@gmail.com>
2024-06-18 23:37:23 +0100

    Create API.ts

---
+++ src/app/API.ts
@@ -0,0 +1,76 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+const BASE_URL = 'https://engenious-server.vercel.app/'; // Update with your backend server URL
+
+// Create an Axios instance with custom configurations
+const api: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Define types for request data and response data
+export interface Job {
+  _id: string;
+  position: string;
+  postedAt: Date;
+  contractType: string;
+  location: string;
+  specialisation: string;
+  salary: string;
+  jobDescription: string;
+  duration: string;
+  responsibilities: string[];
+  skillsExperience: string[];
+}
+
+// API functions
+export const createJob = async (jobData: { position: string, postedAt: Date, contractType: string, location: string, specialisation: string, salary: string, jobDescription: string, duration: string, responsibilities: string[], skillsExperience: string[] }): Promise<Job> => {
+  try {
+    const response: AxiosResponse<Job> = await api.post('/jobs/create', jobData);
+    return response.data;
+  } catch (error) {
+    throw error; // Throw the error message from the server
+  }
+};
+
+export const getJobById = async (jobId: string): Promise<Job | null> => {
+  try {
+    const response: AxiosResponse<{ job: Job }> = await api.get(`/jobs/get/${jobId}`);
+    return response.data.job;
+  } catch (error) {
+    if (error === 404) {
+      return null; 
+    }
+    throw error; // Throw other errors
+  }
+};
+
+export const getAllJobs = async (): Promise<Job[]> => {
+  try {
+    const response: AxiosResponse<{ jobs: Job[] }> = await api.get('/jobs/get');
+    return response.data.jobs;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export const updateJob = async (jobId: string, jobData: { position: string, postedAt: Date, contractType: string, location: string, specialisation: string, salary: string, jobDescription: string, duration: string, responsibilities: string[], skillsExperience: string[] }): Promise<Job> => {
+  try {
+    const response: AxiosResponse<Job> = await api.patch(`/jobs/update/${jobId}`, jobData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteJob = async (jobId: string): Promise<{ message: string }> => {
+  try {
+    const response: AxiosResponse<{ job: Job; message: string }> = await api.delete(`/jobs/delete/${jobId}`);
+    return { message: response.data.message };
+  } catch (error) {
+    throw error;
+  }
+};
