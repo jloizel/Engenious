8339c5433bce1de773bc302b0a7eb2e19368a279
Jack <jmloizel@gmail.com>
2024-10-14 23:10:49 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "../../components/footer/footer";
 import { JobProvider } from "../../components/jobContext/jobContext";
+import { GA } from "../../components/GA/GA";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         {children}
         <Footer/>
       </body>
+      <GA/>
     </html>
   );
 }
